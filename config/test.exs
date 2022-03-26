539bb15ea67003efeffb327fb76949a49586ba32
Alysson porto <craftgames.alysson@gmail.com>
2022-03-25 23:15:42 -0300

    chore: initial commit

---
+++ config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :api, Api.Repo,
+  username: "root",
+  password: "",
+  hostname: "localhost",
+  database: "api_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :api, ApiWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "8brLOFkYiDgaWayUZKJBlvdlHMCTo67SzecWsyI6+3rnSTOvTlPCQWLmPR5QdORl",
+  server: false
+
+# In test we don't send emails.
+config :api, Api.Mailer, adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
---
+++ lib/api/api/user.ex
@@ -0,0 +1,18 @@
+defmodule Api.Api.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :password, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :password])
+    |> validate_required([:username, :password])
+  end
+end
